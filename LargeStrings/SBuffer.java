414835d2c56db30b74c8d3276bf9c25e7aa22881
SHORYA BAJ <jain.shorya11@gmail.com>
2024-02-26 23:36:22 +0530

    String Buffer

---
+++ LargeStrings/SBuffer.java
@@ -0,0 +1,10 @@
+package LargeStrings;
+
+public class SBuffer {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer();
+        sb.append("WeMakeDevs");
+        String str=sb.toString();
+        System.out.println(str);
+    }
+}
