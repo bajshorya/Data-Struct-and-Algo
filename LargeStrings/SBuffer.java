dadc290837abfc3f29aaffa2b704ae363e6d877b
SHORYA BAJ <jain.shorya11@gmail.com>
2024-02-26 23:56:20 +0530

    String buffer methods

--- LargeStrings/SBuffer.java
+++ LargeStrings/SBuffer.java
@@ -2,9 +2,25 @@ package LargeStrings;
 
 public class SBuffer {
     public static void main(String[] args) {
+        //constructor 1
         StringBuffer sb=new StringBuffer();
+
+        //constructor 2
+        StringBuffer sb2=new StringBuffer("Shorya Baj");
+
+        //Constructor 3
+        StringBuffer sb3=new  StringBuffer(30);
+
         sb.append("WeMakeDevs");
+        sb.append(" is great!");
+        sb.insert(2, " Shorya ");
         String str=sb.toString();
         System.out.println(str);
     }
 }
+
+//String Buffer Advantages:-
+//mutable
+//Efficient
+//Thread Safe-slower
+//String Builder is not thread safe
