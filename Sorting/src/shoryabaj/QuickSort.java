e9be790c12f334ea6d99b25368bd9305f8c1a0fc
SHORYA BAJ <jain.shorya11@gmail.com>
2024-03-18 20:14:00 +0530

    quicks sort kkushwaha

--- Sorting/src/shoryabaj/QuickSort.java
+++ Sorting/src/shoryabaj/QuickSort.java
@@ -35,4 +35,33 @@ public class QuickSort {
         arr[high]=temp;
         return i;
     }
+    static void sort(int[] nums, int low, int hi){
+        if (low>=hi) {
+            return;
+        }
+        int s=low;
+        int e=hi;
+        int m=s+(e-s)/2;
+        int pivot=nums[m];
+
+        while (s<=e) {
+            //also a reason why if its already sorted it will not swap 
+           while (nums[s]<pivot) {
+            s++;
+           }    
+           while (nums[e]>pivot) {
+            e--;
+           }    
+           if (s<=e) {
+            int temp =nums[s];
+            nums[s]=nums[e];
+            nums[e]=temp;
+            s++;
+            e--;
+           }
+           //now pivot is at correct index , please sort tow halves now
+           sort(nums,low,e);
+           sort(nums,s,hi);
+        }
+    }
 }
