e3cde95763091d2f1c647148e6fca4b77e3197c0
SHORYA BAJ <jain.shorya11@gmail.com>
2024-04-03 10:31:41 +0530

    removed the comment

--- Recursion/src/shoryaDsaRecursion/Backtracking/Maze.java
+++ Recursion/src/shoryaDsaRecursion/Backtracking/Maze.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Maze {
     public static void main(String[] args) {
-        // System.out.println(count(3, 3));
+        System.out.println(count(3, 3));
         // path("", 3, 3);
         // System.out.println(pathretdiagonal("", 3, 3));
         boolean[][] board={
