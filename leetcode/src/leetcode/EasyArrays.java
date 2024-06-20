a424fb7c9c93d651eb808b7d90603f1b7ce3e799
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-20 22:08:01 +0530

    singleNumber136

--- leetcode/src/leetcode/EasyArrays.java
+++ leetcode/src/leetcode/EasyArrays.java
@@ -199,6 +199,22 @@ public class EasyArrays {
         }
         return max;
     }
+    public static int singleNumber136(int[] nums) {
+        int n =nums.length;
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int cnt=0;
+            for(int j =0;j<n;j++){
+                if(nums[j]==num){
+                    cnt++;
+                }
+            }
+            if (cnt==1){
+                return num;
+            }
+        }
+        return -1;
+    }
      public static void main(String[] args) {
         // subtractProductAndSum(212233);
 int[] testCase1 = {3, 4, 5, 1, 2};
