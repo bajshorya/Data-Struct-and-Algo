120f5d64f94dad50575d8d5a48b088ca28cfa302
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-22 00:03:15 +0530

    75. Sort Colors

--- leetcode/src/leetcode/EasyArrays.java
+++ leetcode/src/leetcode/EasyArrays.java
@@ -215,9 +215,37 @@ public class EasyArrays {
         }
         return -1;
     }
+    public static void sortColors75(int[] nums) {
+        int cnt0=0;
+        int cnt1=0;
+        int cnt2=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==0){
+                cnt0++;
+            }
+            else if(nums[i]==1){
+                cnt1++;
+            }
+            else if(nums[i]==2){
+                cnt2++;
+            }
+        }
+            for (int i = 0; i < cnt0; i++) {
+            nums[i] = 0;
+        }
+
+        for (int i = cnt0; i < cnt0 + cnt1; i++) {
+            nums[i] = 1;
+        }
+
+        for (int i = cnt0 + cnt1; i < nums.length; i++) {
+            nums[i] = 2;
+        }
+    }
+
      public static void main(String[] args) {
         // subtractProductAndSum(212233);
-int[] testCase1 = {3, 4, 5, 1, 2};
+        int[] testCase1 = {3, 4, 5, 1, 2};
         System.out.println(sortedRotated1752(testCase1)); // Expected output: true
         // int[] result = runningSum(nums);
         
