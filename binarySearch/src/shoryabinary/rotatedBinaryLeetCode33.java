e96b2e3283103254b40fcb6e6bfd16978b9f1aab
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-26 00:46:53 +0530

    SearchinRotatedSortedArrayII

--- binarySearch/src/shoryabinary/rotatedBinaryLeetCode33.java
+++ binarySearch/src/shoryabinary/rotatedBinaryLeetCode33.java
@@ -94,5 +94,40 @@ public class rotatedBinaryLeetCode33 {
         return -1;
     }
 
+    public static boolean SearchinRotatedSortedArrayII(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left=left+1;
+                right=right-1;
+                continue;
+            }
+
+            // Determine which part is sorted
+            if (nums[left] <= nums[mid]) {
+                // Left part is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // Right part is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
 
 }
